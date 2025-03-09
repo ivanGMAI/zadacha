a33b2b64490a60c7d622bdf50852d01c30a48512
IVANGG <ivan.gerasimou@yandex.ru>
2025-03-09 18:34:36 +0300

    резюме сеньор

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ books = [
     }
 ]
 
+new_id = len(books)
 
 @app.get('/books', tags=['BOOM'], summary='NABLEVAL')
 def read():
@@ -34,9 +35,14 @@ def read():
 
 @app.get('/books/{book_id}', tags=['BOOM'], summary='NA')
 def get_book(book_id: int):
+    maxid = books[-1]['id']
     for book in books:
         if book['id'] == book_id:
             return book
+    if maxid < book_id:
+        return f'такой нет'
+    elif maxid > book_id:
+        return f'эта книга удалена'
     raise HTTPException(status_code=404, detail='idi gulai')
 
 
@@ -51,7 +57,8 @@ class Newbook(BaseModel):
 def create_book(new_book: Newbook):
     if any(book['title'] == new_book.title for book in books):
         raise HTTPException(status_code=404, detail='уже есть')
-    new_id = len(books) + 1
+    global new_id
+    new_id += 1
     books.append({
         'title': new_book.title,
         'data': new_book.data,
@@ -65,8 +72,11 @@ def delete_book(book_id: int):
     global books
     initial_length = len(books)
     books = [book for book in books if book['id'] != book_id]
+    '''for book in books:
+        book['id'] = books.index(book) + 1'''
     if len(books) == initial_length:
         raise HTTPException(status_code=404, detail='Книга не найдена')
+
     return {'ok': True}
 
 

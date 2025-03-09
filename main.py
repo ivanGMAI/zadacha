c2a83b4baac7ece87fc26a675ee1538968745b7d
IVANGG <ivan.gerasimou@yandex.ru>
2025-03-09 17:25:00 +0300

    books

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ books = [
     {
         'title': 'na zap bes peremen',
         'data': 1925,
-        'pocht': 'sosi@mail.ru',
+        'pocht': 'hohol@mail.ru',
         'id': 1
     },
     {
@@ -32,12 +32,12 @@ def read():
     return books
 
 
-@app.get('/books/{book_id}', tags=['BOOM'], summary='NA NAHUI')
+@app.get('/books/{book_id}', tags=['BOOM'], summary='NA')
 def get_book(book_id: int):
     for book in books:
         if book['id'] == book_id:
             return book
-    raise HTTPException(status_code=404, detail='idi nahui')
+    raise HTTPException(status_code=404, detail='idi gulai')
 
 
 class Newbook(BaseModel):
